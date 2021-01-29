3e0cc7ef47ab13d79811ae861ad749e17e567978
Anthony Ku <anthony.yc.ku@gmail.com>
2021-01-29 09:59:56 -0500

    a

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,22 +1,26 @@
 class CompaniesController < ApplicationController
 
+    # def index
+    #     render json: Company.all
+    # end
+
+    # def show
+    #     render json: Company.find(params["id"])
+    # end
+
+    # def create
+    #     render json: Company.create(params["company"])
+    # end
+
+    # def delete
+    #     render json: Company.delete(params["id"])
+    # end
+
+    # def update
+    #     render json: Company.update(params["id"], params["company"])
+    # end
+
     def index
-        render json: Company.all
-    end
-
-    def show
-        render json: Company.find(params["id"])
-    end
-
-    def create
-        render json: Company.create(params["company"])
-    end
-
-    def delete
-        render json: Company.delete(params["id"])
-    end
-
-    def update
-        render json: Company.update(params["id"], params["company"])
+        render json: "hi"
     end
 end
