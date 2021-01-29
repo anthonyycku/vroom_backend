33dfc21a145bc10bc0db5b89af2408f3835bca7d
Anthony Ku <anthony.yc.ku@gmail.com>
2021-01-29 10:25:34 -0500

    w

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/companies', to:'companies#index'
-  # get '/companies/:id', to:'companies#show'
-  # post '/companies', to:'companies#create'
-  # delete '/companies/:id', to:'companies#delete'
-  # put '/companies/:id', to:'companies#update'
+  get '/companies/:id', to:'companies#show'
+  post '/companies', to:'companies#create'
+  delete '/companies/:id', to:'companies#delete'
+  put '/companies/:id', to:'companies#update'
 
-  # get '/cars', to:'cars#index'
-  # get '/cars/:id', to:'cars#show'
-  # post '/cars', to:'cars#create'
-  # delete '/cars/:id', to:'cars#delete'
-  # put '/cars/:id', to:'cars#update'
+  get '/cars', to:'cars#index'
+  get '/cars/:id', to:'cars#show'
+  post '/cars', to:'cars#create'
+  delete '/cars/:id', to:'cars#delete'
+  put '/cars/:id', to:'cars#update'
 end
