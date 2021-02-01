95878f02538bac4479e3eb2646a733abffd02424
Anthony Ku <anthony.yc.ku@gmail.com>
2021-02-01 15:00:13 -0500

    added

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/companies', to:'companies#index'
-  get '/companies/:id', to:'companies#show'
-  post '/companies', to:'companies#create'
-  delete '/companies/:id', to:'companies#delete'
-  put '/companies/:id', to:'companies#update'
+  get '/companies', to: 'companies#index'
+  get '/companies/:id', to: 'companies#show'
+  post '/companies', to: 'companies#create'
+  put '/companies/:id', to: 'companies#update'
+  delete '/companies/:id', to: 'companies#delete'
 
   get '/cars', to:'cars#index'
+  get '/singleCar/:id', to:'cars#showSingle'
   get '/cars/:id', to:'cars#show'
   post '/cars', to:'cars#create'
   delete '/cars/:id', to:'cars#delete'
   put '/cars/:id', to:'cars#update'
+  # Filter routes
+  get '/filter/type/:id/:type', to:'cars#filterType'
 end
