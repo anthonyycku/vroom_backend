a74e2bf6a44660ed7415c9efe10e7fdb7a0b0e04
Maher George Brikae <mahergeorgebrikae@Mahers-MacBook-Pro.local>
2021-02-02 22:26:54 -0500

    a

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   put '/companies/:id', to:'companies#update'
   get '/filter/country', to:'companies#filterCountry'
   get '/filter/countryDesc', to:'companies#filterCountryDesc'
-  get '/filter/alphabeticalASC', to:'companies#filterASC'
-  get '/filter/alphabeticalDesc', to:'companies#filterDesc'
+  get '/filter/alphabeticalASC', to:'companies#alphabeticalASC'
+  get '/filter/alphabeticalDesc', to:'companies#alphabeticalDesc'
 
   get '/cars', to:'cars#index'
   get '/singleCar/:id', to:'cars#showSingle'
